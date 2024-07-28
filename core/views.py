0529746d5fb69011effddb194f438269380a8050
uzair <smartuzairsahil@gmail.com>
2024-07-28 12:53:20 +0500

    Working system till no permissions is applied

--- core/views.py
+++ core/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from academics.models import Department 
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required 
-from django.urls import resolve, reverse
-from django.utils.text import slugify
+
 
 # Create your views here.
 @login_required
@@ -16,22 +15,3 @@ def Logout(request):
     logout(request)  # Log out the user
     return redirect('login')  # Redirect to the login page
 
-def breadcrumbs(request):
-    url_name = resolve(request.path_info).url_name
-    if not url_name:
-        return {}
-
-    breadcrumbs = [{'name': 'Dashboard', 'url': reverse('dashboard')}]
-
-    # Split the path into components and create breadcrumb items
-    path_components = request.path.strip('/').split('/')
-    for i, component in enumerate(path_components):
-        name = component.replace('_', ' ').capitalize()
-        url = '/' + '/'.join(path_components[:i+1])
-        breadcrumbs.append({'name': name, 'url': url})
-
-    # Mark the last breadcrumb item as active
-    if breadcrumbs:
-        breadcrumbs[-1]['url'] = None
-
-    return {'breadcrumbs': breadcrumbs}
